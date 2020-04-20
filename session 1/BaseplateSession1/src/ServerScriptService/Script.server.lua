56d400fa96811f54e5f20c928213233b62eacb65
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2020-04-20 16:18:53 -0400

    start test proj 1

---
+++ session 1/BaseplateSession1/src/ServerScriptService/Script.server.lua
@@ -0,0 +1,9 @@
+local part = game.Workspace.testPart
+part.Transparency = 0.7
+part.Material = Enum.Material.Sand
+part.Color = Color3.fromRGB(0, 0 ,0)
+function doSomething()
+    print("did something")
+end
+
+doSomething()
