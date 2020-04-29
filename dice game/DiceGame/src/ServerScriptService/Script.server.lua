c07c3c9247f6ead0cbc316d20d1ef4f3d901c5e4
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2020-04-29 15:56:14 -0400

    dice game 1

---
+++ dice game/DiceGame/src/ServerScriptService/Script.server.lua
@@ -0,0 +1,9 @@
+local Workspace = game:GetService("Workspace")
+local dice_1 = Workspace.dice_1
+local dice_2 = Workspace.dice_2
+
+dice_1.Anchored = false
+
+wait(1)
+
+dice_2.Anchored = false
--- session 1/BaseplateSession1/src/ServerScriptService/Script.server.lua
+++ session 1/BaseplateSession1/src/ServerScriptService/Script.server.lua
@@ -2,8 +2,12 @@ local part = game.Workspace.testPart
 part.Transparency = 0.7
 part.Material = Enum.Material.Sand
 part.Color = Color3.fromRGB(0, 0 ,0)
+
 function doSomething()
     print("did something")
 end
 
 doSomething()
+
+local part2 = game.Workspace["A Part"]
+part2.Color = Color3.fromRGB(255, 0 , 0)
